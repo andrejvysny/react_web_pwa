3d18a2ab7cb547470e3f7129e23e23de46d5116e
AndrejVysny <vysnyandrej@gmail.com>
2024-02-05 19:51:22 +0100

    dynamic env docker

--- src/main/views/HomeView.js
+++ src/main/views/HomeView.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
+
+function getEnv(env){
+
+    if(process.env.NODE_ENV === "production"){
+        return window?._env_[env]
+    }else{
+        return 
+    }
+
+}
+
+
 export default function HomeView() {
+
 
     return (
         <>
+         
             <h1 className="text-center mt-5 fonts_size_2rem">React PWA Template</h1>
+            <p>HOST={getEnv("HOST")}</p>
+            <p>NODE_ENV={getEnv("NODE_ENV")}</p>
         </>
     );
 
